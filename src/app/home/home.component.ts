b56764f1bc1d3f29af7c37df06ea312a2867f998
yub0n <y.suzuki@atama.plus>
2023-08-20 16:34:35 +0900

    add: home component

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,11 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [CommonModule],
+  template: ` <p>home works!</p> `,
+  styleUrls: ['./home.component.css'],
+})
+export class HomeComponent {}
