1a982b6898cd79c21fde941faa35f59e425a9cc4
yub0n <y.suzuki@atama.plus>
2023-08-20 16:43:31 +0900

    add: housing location input

--- src/app/housing-location/housing-location.component.ts
+++ src/app/housing-location/housing-location.component.ts
@@ -1,17 +1,14 @@
-import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
+import { HousingLocation } from '../housinglocation';
 
 @Component({
   selector: 'app-housing-location',
   standalone: true,
   imports: [CommonModule],
-  template: `
-    <p>
-      housing-location works!
-    </p>
-  `,
-  styleUrls: ['./housing-location.component.css']
+  template: ` <p>housing-location works!</p> `,
+  styleUrls: ['./housing-location.component.css'],
 })
 export class HousingLocationComponent {
-
+  @Input() housingLocation!: HousingLocation;
 }
